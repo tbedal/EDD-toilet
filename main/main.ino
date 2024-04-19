e0254806575d0a0125c7d107cd1fc9cd950ca568
MilkyIQ <66349361+MilkyIQ@users.noreply.github.com>
2024-04-19 15:47:45 -0500

    Implement time-based motor control function and basic stress test

--- main/main.ino
+++ main/main.ino
@@ -1,103 +1,45 @@
-#define MOTOR_POSITIVE 8 // DC Motor: Positive pin
-#define MOTOR_NEGATIVE 7 // DC Motor: Negative pin
-#define PROXIMITY_TRIG 3 // Proximity Sensor: Ultrasonic pulse sending pin
-#define PROXIMITY_ECHO 2 // Proximity Sensor: Ultrasinic pulse recieving pin
-#define POTENTIOMETER A5 // Potentiometer: Analog input pin
+#include <Servo.h>
 
-const int NUM_SAMPLES = 20;
-const int NUM_OUTLIERS = 5;
-const float ACTIVIATION_THRESHOLD_CM = 10.00;
-int cyclesUserAbsent;
+#define SERVO_PWM 9
 
-// Get data from ultrasonic sensor and convert to distance in centimeters
-float ultrasonicMeasureCM() {
-  digitalWrite(PROXIMITY_TRIG, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(PROXIMITY_TRIG, LOW);
-
-  return 0.017 * pulseIn(PROXIMITY_ECHO, HIGH); // Conversion to CM
-}
-
-// Spin motor clockwise
-void rotateRight() {
-  digitalWrite(MOTOR_NEGATIVE, LOW);
-  digitalWrite(MOTOR_POSITIVE, HIGH);
-}
-
-// Spin motor counter-clockwise
-void rotateLeft() {
-  digitalWrite(MOTOR_NEGATIVE, HIGH);
-  digitalWrite(MOTOR_POSITIVE, LOW);
-}
-
-// Cut power to motor
-void stopRotation() {
-  digitalWrite(MOTOR_NEGATIVE, LOW);
-  digitalWrite(MOTOR_POSITIVE, LOW);
-}
+Servo motor;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(PROXIMITY_TRIG, OUTPUT);
-  pinMode(PROXIMITY_ECHO, INPUT);
-  pinMode(POTENTIOMETER, INPUT);
-  pinMode(MOTOR_POSITIVE, OUTPUT);
-  pinMode(MOTOR_NEGATIVE, OUTPUT);
+  motor.attach(SERVO_PWM);
+  Serial.println("Program initialized.");
+}
+
+// Rotate motor from current angle to angleStop in a set amount of time
+void rotateMotor(int angleStop, unsigned long movingTimeMs) {
+  int angleStart = motor.read();
+  unsigned long moveStartMs = millis();
+
+  while (motor.read() != angleStop) {
+    unsigned long progress = millis() - moveStartMs;
+    long angle = map(progress, 0, movingTimeMs, angleStart, angleStop); // I don't know why this works
+    motor.write(angle);
+  }
 }
 
 void loop() {
-  // Take multiple measurements and store in array
-  float measurementsCM[NUM_SAMPLES];
-  for (int sample = 0; sample < NUM_SAMPLES; sample++) {
-    measurementsCM[sample] = ultrasonicMeasureCM();
-    delay(30); // to avoid ultrasonic interference
+  Serial.println("Starting test...");
+
+  for (int i = 0; i < 10; i++) {
+    Serial.print("Starting cycle #");
+    Serial.print(i);
+    Serial.print("... ");
+
+    rotateMotor(0, 3000);
+    delay(1500);
+    rotateMotor(90, 3000);
+    delay(1500);
+
+    Serial.println("Complete!");
   }
 
-  // Sort measurements in ascending order
-  for (int i = 0; i < NUM_SAMPLES-1; i++) {
-    for (int j = i+1; j < NUM_SAMPLES; j++) {
-      if (measurementsCM[i] > measurementsCM[j]) {
-        measurementsCM[i] = measurementsCM[j];
-        measurementsCM[j] = measurementsCM[i];
-      }
-    }
-  }
+  Serial.print("Test complete!");
 
-  // Get sum of middle values
-  double sum = 0;
-  for (int sample = NUM_OUTLIERS; sample < (NUM_SAMPLES - NUM_OUTLIERS); sample++) {
-    sum += measurementsCM[sample];
-  }
-
-  // Get average of middle samples
-  float distanceCM = sum / (NUM_SAMPLES - (NUM_OUTLIERS * 2));
-  int potAngleDegrees = analogRead(POTENTIOMETER);
-
-  // TODO: this is inefficient. Switch to a rolling average?
-  // Keep track of how long user has been absent
-  cyclesUserAbsent = distanceCM < ACTIVIATION_THRESHOLD_CM ? 0 : cyclesUserAbsent + 1;
-  if (cyclesUserAbsent >= 1000) {
-    cyclesUserAbsent = 3; //make sure cycle count does not reach integer limit
-  }
-
-  // Open, close, or standby toilet lid 
-  if (cyclesUserAbsent > 3 && potAngleDegrees > 500) {
-    rotateLeft();
-    Serial.println("Closing the lid!");
-  }
-  else if (cyclesUserAbsent == 0 && potAngleDegrees < 5) {
-    rotateRight();
-    Serial.println("Opening the lid!");
-  }
-  else {
-    stopRotation();
-    Serial.println("Waiting for movement...");
-  }
-
-  // Serial monitor readout
-  Serial.print(distanceCM);
-  Serial.print("  ");
-  Serial.print(potAngleDegrees);
-  Serial.print("  ");
-  Serial.println(cyclesUserAbsent);
+  delay(2000);
+  exit(0);
 }
